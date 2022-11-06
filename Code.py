385d8f22a5338560d3b34ded2b50e01e4275387c
Dmitriy Birilko <101152412+Sauberr@users.noreply.github.com>
2022-11-06 21:56:21 +0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,53 @@
+# def division(num1, num2, num3 = 1):
+#     if num3:
+#         a = num1 / num2 / 1
+#     else:
+#         a =   num1 / num2
+#     return a
+#
+# print(division(4, 2))
+
+
+#
+# def division(num1, num2):
+#      if type(num1) not in (float, int) or type(num2) not in (float, int):
+#          return 'Type Error'
+#      if num2 == 0:
+#          return 'Division by Zero'
+#      return num1 / num2
+
+
+# def string(a):
+#      b = a
+#      return b
+#
+#
+# print(string(('- Did Joffrey agree?\n- He did. He also said "I love using \\n".')))
+
+
+#
+# def s_1(s):
+#    return(s.lower())
+# #
+# #
+# # if __name__ == '__main__':
+# #     s = input()
+# #     result = s
+# #     print(result)
+#
+# # def  line_1(line):
+# #   line.split()
+# #   return ('-'.join(line))
+#
+# # if __name__ == '__main__':
+# #     line = input()
+# #     result = line_1(line)
+# #     print(result)
+
+# lst = list(map(int, input().split()))
+# lst[-1] = bool(lst[-1] % 2)
+# print(*lst)
+
+# lst = input().split()
+# lst.append(lst[0] != bool(lst[-1]))
+# print(*lst)
